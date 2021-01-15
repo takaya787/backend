e85d3948faabc73e5ec759d1487a3dda27a73239
takaya787 <takaya318318@gmail.com>
2021-01-15 19:57:45 +0900

    postsを消去してusers 作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,9 @@ module Api
       @user = User.new(user_params)
 
       if @user.save
-        render json: @user, status: :created, location: @user
+        render json: @user, status: :created, location: api_user_url(@user)
       else
-        render json: @user.errors, status: :unprocessable_entity
+        render json: { errors: @user.errors}, status: :unprocessable_entity
       end
     end
 
@@ -47,7 +47,7 @@ module Api
 
       # Only allow a list of trusted parameters through.
       def user_params
-        params.require(:user).permit(:name, :email, :password_digest)
+        params.require(:user).permit(:name, :email, :password,:password_confirmation)
       end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
   namespace 'api' do
-    resources :posts
+    resources :users
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
